2f7774c74932da63198c66cd4d09b336ea30e872
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-07-17 20:10:28 +0100

    done with home hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
 'use client'
 
+import ContentSection from '@/components/Home/ContentSection'
 import Header from '@/components/Home/Header'
+import ImageSection from '@/components/Home/ImageSection'
 import { useMediaQuery } from 'react-responsive'
 
 export default function Home() {
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
 
   return (
-    <div>
+    <div className='md:h-screen'>
       <Header isMobile={isMobile} />
 
-      <div></div>
+      <div className='flex flex-col-reverse md:flex-row md:justify-between md:items-center md:h-full'>
+        <ContentSection />
+
+        <ImageSection isMobile={isMobile} />
+      </div>
     </div>
   )
 }
--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -10,7 +10,7 @@ type HeaderProps = {
 
 export default function Header({ isMobile }: HeaderProps) {
   return (
-    <header className='flex items-center justify-between py-3 md:py-4 px-3 md:px-11 bg-white'>
+    <header className='flex items-center justify-between py-3 md:py-4 px-3 md:px-11 bg-white shadow-sm'>
       {isMobile ? <MobileLogo /> : <DesktopLogo />}
 
       <div className='md:flex gap-5 items-center'>
