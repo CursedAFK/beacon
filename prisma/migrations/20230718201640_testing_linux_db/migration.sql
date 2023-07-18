521ab2393647476ef43a519de49a8ca8ed9f961f
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-07-18 21:20:42 +0100

    linux init

--- prisma/migrations/20230718201640_testing_linux_db/migration.sql
+++ prisma/migrations/20230718201640_testing_linux_db/migration.sql
@@ -1,6 +1,43 @@
--- AlterTable
-ALTER TABLE `user` ADD COLUMN `about` VARCHAR(191) NULL,
-    ADD COLUMN `profileImage` VARCHAR(191) NULL;
+-- CreateTable
+CREATE TABLE `User` (
+    `id` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NOT NULL,
+    `hashedPassword` VARCHAR(191) NOT NULL,
+    `occupation` VARCHAR(191) NULL,
+    `role` ENUM('STUDENT', 'MENTOR') NOT NULL DEFAULT 'STUDENT',
+    `profileImage` VARCHAR(191) NULL,
+    `about` VARCHAR(191) NULL,
+
+    UNIQUE INDEX `User_fullName_key`(`fullName`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Achievement` (
+    `id` VARCHAR(191) NOT NULL,
+    `type` ENUM('MENTORSHIP', 'RELATIONSHIP', 'DONATION') NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Event` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `hostId` VARCHAR(191) NOT NULL,
+    `eventDate` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `EventAttendee` (
+    `userId` VARCHAR(191) NOT NULL,
+    `eventId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`userId`, `eventId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `FriendRequest` (
@@ -38,6 +75,18 @@ CREATE TABLE `Chat` (
 
     PRIMARY KEY (`senderId`, `receiverId`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Achievement` ADD CONSTRAINT `Achievement_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Event` ADD CONSTRAINT `Event_hostId_fkey` FOREIGN KEY (`hostId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EventAttendee` ADD CONSTRAINT `EventAttendee_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `EventAttendee` ADD CONSTRAINT `EventAttendee_eventId_fkey` FOREIGN KEY (`eventId`) REFERENCES `Event`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `FriendRequest` ADD CONSTRAINT `FriendRequest_senderId_fkey` FOREIGN KEY (`senderId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
