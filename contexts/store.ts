52bad37498cfb5ce83c996a76e7a256b9179ef5e
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-08-02 16:42:59 +0100

    refactored immer js

--- contexts/store.ts
+++ contexts/store.ts
@@ -1,20 +1,20 @@
 import { User } from '@prisma/client'
-import { produce } from 'immer'
 import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
 type GlobalStore = {
   user: User | undefined
   addUser: (user: User) => void
 }
 
-const useGlobalStore = create<GlobalStore>(set => ({
-  user: undefined,
-  addUser: (user: User) =>
-    set(
-      produce<GlobalStore>(store => {
+const useGlobalStore = create(
+  immer<GlobalStore>(set => ({
+    user: undefined,
+    addUser: (user: User) =>
+      set(store => {
         store.user = user
       })
-    )
-}))
+  }))
+)
 
 export default useGlobalStore
